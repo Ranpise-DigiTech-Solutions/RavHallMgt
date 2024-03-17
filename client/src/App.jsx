b5c7bffd6301361bf65be60865ea79847012092d
Adithya <adikrishna1972@gmail.com>
2024-03-17 16:50:59 +0530

    Minor Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,48 @@
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { Provider } from 'react-redux';
+
 import './App.scss'
 import { 
   HomePage,
+  DescriptionPage,
 } from './pages'
+
+import { store } from './states';
 
 function App() {
 
+  const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+  if (!publishableKey) {
+    throw new Error("Missing Publishable Key")
+  }
+
+  const ClerkProviderWithRoutes = () => {
+    const navigate = useNavigate();
+  
+    return (
+      <ClerkProvider
+        publishableKey={publishableKey}
+        navigate={(to) => navigate(to)}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+          <Routes>
+            <Route path="/DescriptionPage" element={<DescriptionPage />} />
+          </Routes>
+  
+        </ClerkProvider>
+      );
+  }
+
   return (
     <>
-      <HomePage/>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ClerkProviderWithRoutes />
+      </BrowserRouter>
+    </Provider>
     </>
     )
 }
